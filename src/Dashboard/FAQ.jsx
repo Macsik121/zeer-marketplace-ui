f9a4cb66dcbbb0e6719b765649a74e5c4be3600e
Macsik121 <hiomimipad@gmail.com>
2021-07-19 14:45:28 +0300

    deploying

--- src/Dashboard/FAQ.jsx
+++ src/Dashboard/FAQ.jsx
@@ -1,6 +1,31 @@
 import React from 'react';
+import fetchData from '../fetchData';
 
 export default class FAQ extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            answers: []
+        };
+    }
+    async componentDidMount() {
+        const query = `
+            query {
+                getAnswers {
+                    answer
+                    userfulRate
+                    sort {
+                        config
+                        base
+                        paymentActivation
+                    }
+                    title
+                }
+            }
+        `;
+        const result = await fetchData(query);
+        console.log(result.getAnswers);
+    }
     render() {
         return (
             <div className="FAQ">
