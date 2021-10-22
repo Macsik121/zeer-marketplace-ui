16ff18741a2e628a2e8fa08a631c30ac92b8856d
Macsik121 <hiomimipad@gmail.com>
2021-10-22 15:18:14 +0300

    loader downloads much faster

--- src/downloadLoader.js
+++ src/downloadLoader.js
@@ -4,38 +4,40 @@ import jwtDecode from 'jwt-decode';
 import createNotification from './createNotification';
 
 export default async function downloadLoader(link) {
-    let username = jwtDecode(localStorage.getItem('token'));
-    username = username.name;
-    const { user } = await fetchData(`
-        query user($name: String!) {
-            user(name: $name) {
-                subscriptions {
-                    status {
-                        isActive
-                    }
-                }
-            }
-        }
-    `, { name: username });
-    let userHasActiveSubs = false;
-    for(let i = 0; i < user.subscriptions.length; i++) {
-        console.log(user.subscriptions[i].status.isActive)
-        if (user.subscriptions[i].status.isActive) {
-            userHasActiveSubs = true;
-            break;
-        }
-    }
-    if (!userHasActiveSubs) {
-        createNotification('error', 'У вас нету ни одной активной подписки');
-        return;
-    }
     let result = await fetch(__UI_SERVER_ENDPOINT__ + '/loader.exe', {
         method: 'POST'
     });
     result = await result.blob();
     const url = window.URL.createObjectURL(result);
     let clickLink = false;
-    if (link.download == '') clickLink = true;
+    let userHasActiveSubs = true;
+    if (link.download == '') {
+        clickLink = true;
+    } else {
+        let username = jwtDecode(localStorage.getItem('token'));
+        username = username.name;
+        const { user } = await fetchData(`
+            query user($name: String!) {
+                user(name: $name) {
+                    subscriptions {
+                        status {
+                            isActive
+                        }
+                    }
+                }
+            }
+        `, { name: username });
+        for(let i = 0; i < user.subscriptions.length; i++) {
+            if (user.subscriptions[i].status.isActive) {
+                userHasActiveSubs = true;
+                break;
+            }
+        }
+    }
+    if (!userHasActiveSubs) {
+        createNotification('error', 'У вас нету ни одной активной подписки');
+        return;
+    };
     link.download = 'loader.exe';
     link.href = url;
     if (clickLink) link.click();
